0eda2a299343336a544df8f4e1c62af27def9c74
Dawson <HawkHogg@gmail.com>
2025-08-30 18:30:29 +0800

    rectification

--- apps/oaauth/management/commands/initdepartments.py
+++ apps/oaauth/management/commands/initdepartments.py
@@ -13,13 +13,19 @@ from apps.oaauth.models import OADepartment
 
 
 class Command(BaseCommand):
-    """ 初始化部门数据 """
+    """
+    初始化部门数据:
+    董事会, 财务部, 人事部, 产品开发部, 运营部, 销售部
+    """
 
     def handle(self, *args, **options):
-        director = OADepartment.objects.post()
-        finance = OADepartment.objects.post()
-        hr = OADepartment.objects.post()
-        developer = OADepartment.objects.post()
-        operator = OADepartment.objects.post()
-        saler = OADepartment.objects.post()
+        departments = [
+            OADepartment(name="董事会", intro="董事会部门"),
+            OADepartment(name="财务部", intro="财务报表，财务审核"),
+            OADepartment(name="人事部", intro="员工招聘，员工培训，员工考核"),
+            OADepartment(name="产品开发部", intro="产品设计，技术开发"),
+            OADepartment(name="运营部", intro="客户运营，产品运营"),
+            OADepartment(name="销售部", intro="产品销售"),
+        ]
+        OADepartment.objects.bulk_create(departments)
         return self.stdout.write("部门数据初始化成功!")
