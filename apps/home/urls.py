f3b12ca1ada15505ec5a1c851633f42c8c2ecdd8
Dawson <HawkHogg@gmail.com>
2025-09-03 16:30:59 +0800

    add path to HealthCheck API

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('latest/inform',views.LatestInformView.as_view(), name='latest_inform'),
     path('latest/absent',views.LatestAbsenceView.as_view(), name='latest_absent'),
     path('department/staff/count',views.DepartmentStaffView.as_view(), name='department_staff_count'),
+    path('health',views.HealthCheckView.as_view(), name='health_check'),
 ]
